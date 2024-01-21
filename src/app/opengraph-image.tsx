3367a5eddb03c1aba4058d78503bee21cbae854a
Filip John <filip@frend.no>
2024-01-21 19:38:51 +0100

    og update

--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -46,13 +46,23 @@ export default async function Image() {
         </p>
         <p
           style={{
-            fontSize: 20,
+            fontSize: 50,
             background: "#dc2626",
 
             color: "white",
           }}
         >
-          Med renter fra {account.interestRate.interest} %
+          <span
+            style={{
+              fontSize: 20,
+              background: "#dc2626",
+
+              color: "white",
+            }}
+          >
+            Med renter fra{" "}
+          </span>{" "}
+          {account.interestRate.interest} %
         </p>
       </div>
     ),
